ad14fc035103487bda318a3c489c6143de73c3ab
Pedro Telón <pedrixtelon@gmail.com>
2021-11-26 12:35:40 -0600

    Primer problema hecho

--- practicas.js
+++ practicas.js
@@ -1,1 +1,39 @@
-//Problema 1
+
+
+///Problema 1
+function Calcular(string,x){
+string = "Caca de mono";
+x = string.length
+console.log(x)
+return x;
+}
+
+Calcular();
+
+
+
+/*
+let string;
+string = "Hola ";
+//Estamos sumando una variable string 
+let w = string.length;
+console.log(w)
+
+/*
+//Ejemplo de la red
+var x = 'Mozilla';
+var empty = '';
+
+console.log('Mozilla is ' + x.length + ' code units long');
+ "Mozilla is 7 code units long" 
+
+console.log('The empty string has a length of ' + empty.length);
+
+var myString = "bluebells";
+
+// Attempting to assign a value to a string's .length property has no observable effect.
+let t = myString.length;
+console.log(t);
+
+*/
+///Problema2
