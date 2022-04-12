fb537232731f154ed08833208f6fac6f1dda684a
Pedro Telón <legendinascent@gmail.com>
2022-04-12 09:53:34 -0600

    Class 87

--- DOM/Ejercicios Practicos/js/dom.js
+++ DOM/Ejercicios Practicos/js/dom.js
@@ -1,12 +1,16 @@
 import hamburgerMenu from "./menuguesa.js";
 import {digitalClock,alarm} from "../js/reloj.js"; 
 import {moveBall,shortcurts }  from "./keyboard.js";
+import countdown from "./countdown.js";
 const d = document;
 d.addEventListener("DOMContentLoaded",(e)=>{
     hamburgerMenu(".panel-btn",".panel",".menu a");
     digitalClock("#reloj","#activar-relog","#desactivar-relog");
     alarm("assets/alarm.mp3","#activar-alarma","#desactivar-alarma");
-
+    countdown(
+        "countdown",
+     "May 12,2050 3:23:19",
+     "Feliz Cumpleaños amigo y docente digital")
 });
 
 d.addEventListener("keydown", e=>{
